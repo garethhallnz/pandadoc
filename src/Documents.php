25f8df904266a7c987bf75a810f8b3963647aa70
raveena <raveena@communica.co.nz>
2017-12-11 09:08:33 +1300

    Function for refresh token

--- src/Documents.php
+++ src/Documents.php
@@ -12,154 +12,176 @@ use PandaDoc\Entity\DocumentBuilder;
  */
 class Documents extends PandaDoc
 {
-    /**
-     * REST resource.
-     */
-    const RESOURCE = '/documents';
+  /**
+   * REST resource.
+   */
+  const RESOURCE = '/documents';
 
-    /**
-     * List Documents.
-     *
-     * @param array $filters
-     *  An associative array of additional options and may contain the following elements.
-     *  - 'q': (string) Optional search query. Filter by document reference number or name.
-     *  - 'tag': (string) Optional search tag. Filter by document tag.
-     *  - 'count': (int) Optionally specify how many document results to return.
-     *  - 'page': (int) Optionally specify which page of the dataset to return.
-     *  - 'metadata': (string) Optionally specify metadata to filter by in the format
-     *    of metadata_{metadata-key}={metadata-value} The metadata_ prefix is always required.
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#list-documents
-     */
-    public function list(array $filters = []): \stdClass
-    {
-        return $this->request('GET', self::RESOURCE, $filters);
-    }
+  /**
+   * List Documents.
+   *
+   * @param array $filters
+   *  An associative array of additional options and may contain the following elements.
+   *  - 'q': (string) Optional search query. Filter by document reference number or name.
+   *  - 'tag': (string) Optional search tag. Filter by document tag.
+   *  - 'count': (int) Optionally specify how many document results to return.
+   *  - 'page': (int) Optionally specify which page of the dataset to return.
+   *  - 'metadata': (string) Optionally specify metadata to filter by in the format
+   *    of metadata_{metadata-key}={metadata-value} The metadata_ prefix is always required.
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#list-documents
+   */
+  public function list(array $filters = []): \stdClass
+  {
+    return $this->request('GET', "/public/v1" . self::RESOURCE, $filters);
+  }
 
-    /**
-     * Document Details.
-     *
-     * @param string $id
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#document-details
-     */
-    public function details(string $id): \stdClass
-    {
-        return $this->request('GET', self::RESOURCE . "/{$id}/details");
-    }
+  /**
+   * Document Details.
+   *
+   * @param string $id
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#document-details
+   */
+  public function details(string $id): \stdClass
+  {
+    return $this->request('GET', "/public/v1" . self::RESOURCE . "/{$id}/details");
+  }
 
-    /**
-     * Document Status.
-     *
-     * @param string $id
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#document-status
-     */
-    public function status(string $id): \stdClass
-    {
-        return $this->request('GET', self::RESOURCE . "/{$id}");
-    }
+  /**
+   * Document Status.
+   *
+   * @param string $id
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#document-status
+   */
+  public function status(string $id): \stdClass
+  {
+    return $this->request('GET', "/public/v1" . self::RESOURCE . "/{$id}");
+  }
 
-    /**
-     * Download Document.
-     *
-     * @param string $id
-     * @param string $destination
-     *
-     * @see Documents::document()
-     * @see https://developers.pandadoc.com/v1/reference#download-document
-     */
-    public function download(string $id, string $destination)
-    {
-        $document = $this->details($id);
+  /**
+   * Download Document.
+   *
+   * @param string $id
+   * @param string $destination
+   *
+   * @see Documents::document()
+   * @see https://developers.pandadoc.com/v1/reference#download-document
+   */
+  public function download(string $id, string $destination)
+  {
+    $document = $this->details($id);
 
-        $path = "{$destination}/{$document->name}.pdf";
+    $path = "{$destination}/{$document->name}.pdf";
 
-        $this->request('GET', self::RESOURCE . "/{$id}/download", ['sink' => $path]);
-    }
+    $this->request('GET', "/public/v1" . self::RESOURCE . "/{$id}/download", ['sink' => $path]);
+  }
 
-    /**
-     * Create Document From a Template.
-     *
-     * @param \PandaDoc\Entity\DocumentBuilder $document
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#new-document
-     */
-    public function createFromTemplate(DocumentBuilder $document): \stdClass
-    {
-        return $this->request('POST', self::RESOURCE, ['json' => $document->getData()]);
-    }
+  /**
+   * Create Document From a Template.
+   *
+   * @param \PandaDoc\Entity\DocumentBuilder $document
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#new-document
+   */
+  public function createFromTemplate(DocumentBuilder $document): \stdClass
+  {
+    return $this->request('POST', "/public/v1" .self::RESOURCE , ['json' => $document->getData()]);
+  }
 
-    /**
-     * Create Document From a Pdf.
-     *
-     * @param \PandaDoc\Entity\DocumentBuilder $document
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#new-document
-     */
-    public function createFromPdf(DocumentBuilder $document): \stdClass
-    {
-        // @todo
-    }
+  /**
+   * Create Document From a Pdf.
+   *
+   * @param \PandaDoc\Entity\DocumentBuilder $document
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#new-document
+   */
+  public function createFromPdf(DocumentBuilder $document): \stdClass
+  {
+    // @todo
+  }
 
 
-    /**
-     * Send Document.
-     *
-     * @param string $id
-     * @param string $message
-     * @param bool $silent
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#send-document
-     */
-    public function send(string $id, string $message, $silent = false): \stdClass
-    {
-        return $this->request(
-            'POST',
-            self::RESOURCE . "/{$id}/send",
-            [
+  /**
+   * Send Document.
+   *
+   * @param string $id
+   * @param string $message
+   * @param bool $silent
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#send-document
+   */
+  public function send(string $id, string $message, $silent = false): \stdClass
+  {
+    return $this->request(
+        'POST',
+        "/public/v1" . self::RESOURCE . "/{$id}/send",
+        [
             'json' => [
-              'message' => $message,
-              'silent' => $silent,
+                'message' => $message,
+                'silent' => $silent,
             ]
-            ]
-        );
-    }
+        ]
+    );
+  }
 
-    /**
-     * Create a Document Link.
-     *
-     * @param string $id
-     * @param string $recipient
-     * @param $lifetime
-     *
-     * @return \stdClass
-     *
-     * @see https://developers.pandadoc.com/v1/reference#link-to-a-document
-     */
-    public function share(string $id, string $recipient, $lifetime): \stdClass
-    {
-        return $this->request(
-            'POST',
-            self::RESOURCE . "/{$id}/session",
-            [
+  /**
+   * Create a Document Link.
+   *
+   * @param string $id
+   * @param string $recipient
+   * @param $lifetime
+   *
+   * @return \stdClass
+   *
+   * @see https://developers.pandadoc.com/v1/reference#link-to-a-document
+   */
+  public function share(string $id, string $recipient, $lifetime): \stdClass
+  {
+    return $this->request(
+        'POST',
+        "/public/v1" . self::RESOURCE . "/{$id}/session",
+        [
             'json' => [
-              'recipient' => $recipient,
-              'lifetime' => $lifetime,
+                'recipient' => $recipient,
+                'lifetime' => $lifetime,
             ]
+        ]
+    );
+  }
+
+  /**
+   * Add data for refresh token
+   */
+  public function addData($client_id,$client_secret,$refresh_token){
+
+    return $this->request_token(
+
+        'POST',
+        "/oauth2/access_token",
+        [
+            'body' => [
+                'grant_type'=>'refresh_token',
+                'client_id'=>$client_id,
+                'client_secret'=>$client_secret,
+                'refresh_token'=>$refresh_token,
+                'scope'=>'read+write',
             ]
-        );
-    }
+        ]
+    );
+  }
+
 }
--- src/PandaDoc.php
+++ src/PandaDoc.php
@@ -13,91 +13,116 @@ use GuzzleHttp\Exception\RequestException;
  */
 abstract class PandaDoc
 {
-    /**
-     * API url.
-     *
-     * @var string
-     */
-    protected $endpoint = 'https://api.pandadoc.com/public/v1';
+  /**
+   * API url.
+   *
+   * @var string
+   */
+  protected $endpoint = 'https://api.pandadoc.com';
 
-    /**
-     * API access token.
-     *
-     * @var string
-     */
-    protected $token;
+  /**
+   * API access token.
+   *
+   * @var string
+   */
+  protected $token;
 
-    /**
-     * Guzzle client.
-     *
-     * @var Client
-     */
-    protected $client;
+  /**
+   * Guzzle client.
+   *
+   * @var Client
+   */
+  protected $client;
 
-    /**
-     * PandaDoc constructor.
-     *
-     * @param string $token
-     * @param Client $client
-     */
-    public function __construct($token = '', Client $client = null)
-    {
-        $this->token = $token;
+  /**
+   * PandaDoc constructor.
+   *
+   * @param string $token
+   * @param Client $client
+   */
+  public function __construct($token = '', Client $client = null)
+  {
+    $this->token = $token;
 
-        if (empty($client)) {
-            $this->client = new Client();
-        } else {
-            $this->client = $client;
-        }
+    if (empty($client)) {
+      $this->client = new Client();
+    } else {
+      $this->client = $client;
     }
+  }
 
-    /**
-     * Makes a request to the PandaDoc API.
-     *
-     * @param $method
-     * @param $resource
-     * @param array $options
-     * @return mixed
-     */
-    public function request(string $method, string $resource, array $options = []): \stdClass
-    {
-        $headers = [
-          'headers' => [
+  /**
+   * Makes a request to the PandaDoc API.
+   *
+   * @param $method
+   * @param $resource
+   * @param array $options
+   * @return mixed
+   */
+  public function request(string $method, string $resource, array $options = []): \stdClass
+  {
+    $headers = [
+        'headers' => [
             'Authorization' => 'Bearer ' . $this->token,
-          ],
-        ];
+        ],
+    ];
 
-        $options = array_merge_recursive($headers, $options);
+    $options = array_merge_recursive($headers, $options);
 
-        if (!empty($options['query'])) {
-            $options['query'] = http_build_query(['query']);
-        }
-
-        return $this->handleRequest($method, $this->endpoint . $resource, $options);
+    if (!empty($options['query'])) {
+      $options['query'] = http_build_query(['query']);
     }
 
-    /**
-     * Makes a request to the PandaDoc API using the Guzzle HTTP client.
-     *
-     * @param $method
-     * @param string $uri
-     * @param array $options
-     * @return mixed
-     * @throws PandaDocAPIException
-     *
-     * @see PandaDoc::request()
-     */
-    public function handleRequest(string $method, string $uri, array $options = []): \stdClass
-    {
-        try {
-          $request = $this->client->createRequest($method, $uri, $options);
-          $response = $this->client->send($request);
-          $data = $response->getBody();
-          return json_decode($data->getContents());
-        } catch (RequestException $e) {
-            $response = $e->getResponse();
+    return $this->handleRequest($method, $this->endpoint . $resource, $options);
+  }
 
-            throw new PandaDocAPIException($response, $e);
-        }
+  /**
+   * Makes a request for token update to the PandaDoc API.
+   *
+   * @param $method
+   * @param $resource
+   * @param array $options
+   * @return mixed
+   */
+  public function request_token(string $method, string $resource, array $options = []): \stdClass
+  {
+    $headers = [
+        'headers' => [
+            'Content-type' => 'application/x-www-form-urlencoded',
+        ],
+    ];
+
+    $options = array_merge_recursive($headers, $options);
+
+    if (!empty($options['query'])) {
+      $options['query'] = http_build_query(['query']);
     }
+
+    return $this->handleRequest($method, $this->endpoint . $resource, $options);
+  }
+
+  /**
+   * Makes a request to the PandaDoc API using the Guzzle HTTP client.
+   *
+   * @param $method
+   * @param string $uri
+   * @param array $options
+   * @return mixed
+   * @throws PandaDocAPIException
+   *
+   * @see PandaDoc::request()
+   */
+  public function handleRequest(string $method, string $uri, array $options = []): \stdClass
+  {
+    try {
+      $request = $this->client->createRequest($method, $uri, $options);
+      $response = $this->client->send($request);
+      $data = $response->getBody();
+      return json_decode($data->getContents());
+    } catch (RequestException $e) {
+      $response = $e->getResponse();
+
+      throw new PandaDocAPIException($response, $e);
+    }
+  }
 }
